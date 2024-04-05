5a79a9fbbf10e1e61dd42b62dffce3db35f58c65
Yash Chandra <158017591+Yash-36@users.noreply.github.com>
2024-04-05 22:34:07 +0530

    Add files via upload

---
+++ 2/WAP to determine the roots of the equati.py
@@ -0,0 +1,2 @@
+# WAP to determine the roots of the equation ax2+bx+c=0 in python without function.
+def quadratic_formula(a, b, c)
---
+++ 8/demo.py
@@ -0,0 +1,5 @@
+num=int(input("Enter any number : "))
+if (num % 2) == 0:
+    print ("Even")
+else:
+    print ("Odd")
